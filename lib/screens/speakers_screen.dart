a807f233754343ed626e18d00ed378fa2ba2fe3f
David ROCHELET <drochelet@groupeisgri.com>
2023-04-26 13:32:06 +0200

    Add link to linkedin from the speakers page

--- lib/screens/speakers_screen.dart
+++ lib/screens/speakers_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:seminaire/shared/menu_bottom.dart';
 import '../shared/menu_drawer.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SpeakersScreen extends StatelessWidget {
   const SpeakersScreen({Key? key}) : super(key: key);
@@ -18,7 +19,7 @@ class SpeakersScreen extends StatelessWidget {
           appBar: AppBar(
             bottom: const TabBar(
               tabs: [
-                Tab(text: 'Jeudi 4'),
+                Tab(text: 'Exercice 2022-23'),
               ],
             ), // TabBar
             title: const Text('Les intervenants'),
@@ -55,43 +56,76 @@ class ListViewHSpeakers extends StatelessWidget {
     "Pascal Perez",
     "Nicolas Polychronopoulos",
     "David Rochelet",
-    "Jean-Marc Fernandez"
+    "Jean-Marc Fernandez",
+    "Laura Lange",
+    "Yohan Remi",
+    "Thierry Hardion"
   ];
   final subtitles = [
     "linkedIn",
     "David ... c'est David :)\nbla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla\nbla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla ",
     "On ne présente plus Nicolas,\nil animera les keynotes",
     "linkedIn",
-    "linkedIn"
+    "linkedIn",
+    "linkedIn",
+    "linkedIn",
+    "linkedIn",
   ];
   final photos = [
     "https://media.licdn.com/dms/image/C4D03AQF8l72lDw1w8Q/profile-displayphoto-shrink_200_200/0/1610274294913?e=1687392000&v=beta&t=xa3n0MJW08uvfdAiQZhMmlE73e_F1Fv3pRfuX_1bRQs",
     "https://media.licdn.com/dms/image/C4D03AQE2ceTht-bxtw/profile-displayphoto-shrink_200_200/0/1616663620453?e=1687392000&v=beta&t=vGoEmG_DwVbR8LmJQdK87W-eMY79MnBMUyQVb6qgiKk",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK1nHNC8VuE70TL4SREVCeFZDcZWFMt68tDfqH_JrwIg&s",
     "",
-    ""
+    "",
+    "",
+    "",
+    "",
   ];
   final icons = [
     Icons.security_sharp,
     Icons.security_sharp,
     Icons.developer_board,
     Icons.developer_board,
-    Icons.developer_board
+    Icons.developer_board,
+    Icons.developer_board,
+    Icons.developer_board,
+    Icons.developer_board,
   ];
-
+  final urlToOpen = [
+    "https://www.linkedin.com/",
+    "https://www.linkedin.com/in/david-rochelet-048834ab/",
+    "https://www.linkedin.com/",
+    "https://www.linkedin.com/",
+    "https://www.linkedin.com/",
+    "https://www.linkedin.com/",
+    "https://www.linkedin.com/",
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
+        padding: EdgeInsets.all(20),
         separatorBuilder: (BuildContext context, int index) => const Divider(),
         itemCount: titles.length,
         itemBuilder: (context, index) {
           return Card(
-              child: ListTile(
-                  title: Text(titles[index]),
-                  subtitle: Text(subtitles[index]),
-                  leading: CircleAvatar(
-                      backgroundImage: NetworkImage(photos[index])),
-                  trailing: Icon(icons[index])));
+              child: InkWell(
+                  onTap: () => _launchURL(urlToOpen[index]),
+                  //child: new Text('Ouvrir dans Maps'),
+                  child: ListTile(
+                      title: Text(titles[index]),
+                      subtitle: Text(subtitles[index]),
+                      leading: CircleAvatar(
+                          backgroundImage: NetworkImage(photos[index])),
+                      trailing: Icon(icons[index]))));
         });
+  }
+
+  _launchURL(url) async {
+    final uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
