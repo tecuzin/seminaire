dc990a7471716b8a69dc1a70c7d5523644eef98d
David ROCHELET <drochelet@groupeisgri.com>
2023-05-04 21:54:59 +0200

    Update on friday's agenda

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -16,16 +16,15 @@ class Assets {
   static const String dataPlaces = 'assets/data/places.csv';
   static const String picsDrochelet = 'assets/pics/drochelet.jpg';
   static const String picsHomebg = 'assets/pics/homebg.jpg';
-  static const String picsIsagriLogoComplet =
-      'assets/pics/isagriLogoComplet.jpg';
+  static const String picsIsagriLogoComplet = 'assets/pics/isagriLogoComplet.jpg';
   static const String picsJMF = 'assets/pics/JMF.png';
   static const String picsLlange = 'assets/pics/llange.png';
   static const String picsLogoISAGRI = 'assets/pics/logo-ISAGRI.webp';
   static const String picsLpirard = 'assets/pics/lpirard.png';
-  static const String picsNpolychronopoulos =
-      'assets/pics/npolychronopoulos.jpg';
+  static const String picsNpolychronopoulos = 'assets/pics/npolychronopoulos.jpg';
   static const String picsPperez = 'assets/pics/pperez.jpg';
   static const String picsSpeaker = 'assets/pics/speaker.webp';
   static const String picsThardion = 'assets/pics/thardion.png';
   static const String picsYremi = 'assets/pics/yremi.jpg';
+
 }
